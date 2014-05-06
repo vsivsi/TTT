fa6cf99cf4909c09e23fe41ca3d18e447637b0ff
Vaughn Iverson <vsivsi@yahoo.com>
2014-05-05 20:26:25 -0700

    Simplify test cases

--- tests.js
+++ tests.js
@@ -4,40 +4,7 @@
 ###     See included LICENSE file for details.
 ***************************************************************************/
 
-Tinytest.add('Collection insert and findOne with _id', function(test) {
-  var _id = testColl.insert({ });
-  var doc = testColl.findOne({_id : _id});
-  test.notEqual(doc, undefined, "findOne returned no result.");
-});
-
-Tinytest.addAsync('fileCollection insert and findOne with _id in callback', function(test, onComplete) {
-  var _id = testColl.insert({ }, function (err, retid) {
-
-    if (err) { test.fail(err); }
-    test.equal(retid, _id, "findOne returned bad _id to callback");
-
-    var doc = testColl.findOne({_id : retid});
-    test.notEqual(doc, undefined, "findOne returned no result.");
-    onComplete();
-
-  });
-});
-
-Tinytest.addAsync('fileCollection insert and findOne with _id in callback, wait 500ms', function(test, onComplete) {
-  var _id = testColl.insert({ }, function (err, retid) {
-
-    if (err) { test.fail(err); }
-    test.equal(retid, _id, "findOne returned bad _id to callback");
-
-    Meteor.setTimeout(function () {
-      var doc = testColl.findOne({_id : retid});
-      test.notEqual(doc, undefined, "findOne returned no result.");
-      onComplete();
-    }, 250);
-  });
-});
-
-Tinytest.addAsync('fileCollection insert and findOne with _id in callback, again', function(test, onComplete) {
+var asyncTest = function(test, onComplete) {
   var _id = testColl.insert({ }, function (err, retid) {
 
     if (err) { test.fail(err); }
@@ -47,4 +14,25 @@ Tinytest.addAsync('fileCollection insert and findOne with _id in callback, again
     test.notEqual(doc, undefined, "findOne returned no result.");
     onComplete();
   });
-});
+}
+
+// FAILS
+Tinytest.addAsync('fileCollection insert and findOne with _id in callback', asyncTest);
+
+// PASSES
+Tinytest.addAsync('fileCollection insert and findOne with _id in callback, again', asyncTest);
+
+
+//// IF the code below is the only test, it will pass for timeout delays > ~50ms and fail for timeouts less than that.
+
+// Tinytest.addAsync('fileCollection insert and findOne with _id in callback, wait 500ms', function(test, onComplete) {
+//   var _id = testColl.insert({ }, function (err, retid) {
+//     if (err) { test.fail(err); }
+//     test.equal(retid, _id, "findOne returned bad _id to callback");
+//     Meteor.setTimeout(function () {
+//       var doc = testColl.findOne({_id : retid});
+//       test.notEqual(doc, undefined, "findOne returned no result.");
+//       onComplete();
+//     }, 250);
+//   });
+// });
